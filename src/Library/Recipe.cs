fa7376336b8708656fd34f923223561f286f1237
Santiagoal1208 <santiagoalmagro1208@gmail.com>
2022-09-14 20:45:25 -0300

    solucionado?

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -11,7 +11,7 @@ using System.Text.Json.Serialization;
 
 namespace Recipies
 {
-    public class Recipe
+    public class Recipe 
     {
         // private ArrayList steps = new ArrayList();
         public Product FinalProduct { get; set; }
@@ -28,6 +28,11 @@ namespace Recipies
         public void RemoveStep(Step step)
         {
             this.Steps.Remove(step);
+        }
+        
+        public string ConvertToJson()
+        {
+            return JsonSerializer.Serialize(this);
         }
     }
 }
